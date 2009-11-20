939b186167f0f181d013351958e4a6f4636151c9
Jakub Šťastný aka Botanicus <knava.bestvinensis@gmail.com>
2009-11-20 22:11:34 +0000

    Parameters for pupu (theme, syntax)

--- config.rb
+++ config.rb
@@ -2,13 +2,18 @@
 dependency :mootools
 
 # load javascripts
-javascript "lighter"
-
-# load stylesheets
-stylesheet "lighter"
+javascript "Lighter"
 
 # parameters of plugin
-# it will be blank in most cases
-#parameter :request, :optional => ["local", "ajax"] do |type|
-#  javascript "lib/autocompleter.#{type}"
-#end
+themes   = Dir["#{self.root}/stylesheets/Flame.*.css"].map { |theme| File.basename(theme).split(".")[1] }
+syntaxes = Dir["#{self.root}/javascripts/Fuel.*.js"].map { |theme| File.basename(theme).split(".")[1] }
+
+# pupu :lighter, theme: "mocha"
+parameter :theme, :optional => themes do |theme|
+ stylesheet "Flame.#{theme}"
+end
+
+# pupu :lighter, syntax: "ruby"
+parameter :syntax, :optional => syntaxes do |syntax|
+ javascript "Fuel.#{syntax}"
+end
