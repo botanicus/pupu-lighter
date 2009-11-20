be4cf766506d5025fbed263fbd77973563d892d3
Jakub Šťastný aka Botanicus <knava.bestvinensis@gmail.com>
2009-11-20 21:46:15 +0000

    Initial import

---
+++ config.rb
@@ -0,0 +1,14 @@
+# dependencies of the pupu
+dependency :mootools
+
+# load javascripts
+javascript "lighter"
+
+# load stylesheets
+stylesheet "lighter"
+
+# parameters of plugin
+# it will be blank in most cases
+#parameter :request, :optional => ["local", "ajax"] do |type|
+#  javascript "lib/autocompleter.#{type}"
+#end
